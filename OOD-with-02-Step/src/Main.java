75d6cf4acdc700765fe88828d82af2811dfb0c6c
mssn83 <sadqynzhadm9@gmail.com>
2025-12-01 18:01:50 +0330

    🎨updatig main file

--- OOD-with-02-Step/src/Main.java
+++ OOD-with-02-Step/src/Main.java
@@ -2,17 +2,22 @@ import constants.PaymentMethods;
 import models.Customer;
 import models.LuxuryRoom;
 import constants.Notifier;
+import services.NotificationFactory;
+import services.PaymentProcessor;
 import services.Reservation;
 import models.Room;
 import services.ReservationService;
 
 public class Main {
     public static void main(String[] args){
-        Customer customer = new Customer("Ali", "ali@example.com","09124483765", "Paris");
+        Customer customer = new Customer("Ali", "alikazempour83@gmail.com","09124483765", "Paris");
         Room room = new LuxuryRoom("203", 150);
         Reservation res = new Reservation(room, customer, 2);
 
-        ReservationService service = new ReservationService();
-        service.makeReservation(res, PaymentMethods.PAYPAL, Notifier.EMAIL);
+        PaymentProcessor paymentProcessor = new PaymentProcessor();
+        NotificationFactory notificationFactory = new NotificationFactory();
+
+        ReservationService service = new ReservationService(paymentProcessor, notificationFactory);
+        service.makeReservation(res, PaymentMethods.PAYPAL, Notifier.SMS);
     }
 }
